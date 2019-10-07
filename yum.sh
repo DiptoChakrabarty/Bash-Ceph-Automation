f77ba96576c5e12bab6e5bac352243a1d28d8266
Dipto Chakrabarty <diptochuck123@gmail.com>
2019-10-08 01:37:57 +0530

    added client

--- yum.sh
+++ yum.sh
@@ -5,3 +5,7 @@ do
 	scp /etc/yum.repos.d/ceph.repo  node"$i":/etc/yum.repos.d/ceph.repo
 	scp  /etc/hosts    node"$i":/etc/hosts  
 done
+
+scp /etc/yum.repos.d/ceph.repo  client:/etc/yum.repos.d/ceph.repo
+scp  /etc/hosts    client:/etc/hosts
+
